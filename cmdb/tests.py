765fd650755237e2358bc84a50ccb10537a2bbe3
sunhuang2005 <sunhuang2005@gmail.com>
2016-07-22 14:16:16 +0800

    Basic view now returns minimal HTML

--- cmdb/tests.py
+++ cmdb/tests.py
@@ -1,6 +1,17 @@
 from django.test import TestCase
-
+from django.core.urlresolvers import resolve
+from cmdb.views import homepage
+from django.http import HttpRequest
 # Create your tests here.
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1+1,3)
+class HomePageTest(TestCase):
+    def test_root_url_resolvess_to_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func,homepage)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+
+        response = homepage(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
